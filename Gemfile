421431296eb290cfb0a8922ccf6413fc5a4f3e10
Dan Tylenda-Emmons <DanTylendaEmmons@gmail.com>
2012-01-14 11:19:46 -0600

    added spec test for UsersController

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ end
 group :test do
 	gem 'rspec', '2.6.0'
 	gem 'webrat', '0.7.1'
+	gem 'factory_girl_rails', '1.0'
 end
 
 group :production do
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,7 +3,26 @@ require 'spec_helper'
 describe UsersController do
   render_views
 
+  describe "GET 'show'" do
+
+    before(:each) do
+      @user = Factory(:user)
+    end 
+
+    it "should be successful" do
+      get 'show', :id=>@user
+      response.should be_success
+    end
+
+    it "should get the right user" do
+      get "show", :id=>@user
+      assigns(:user).should == @user # note: 'assigns' returns the controller variable
+    end
+
+  end
+
   describe "GET 'new'" do
+
     it "should be successful" do
       get 'new'
       response.should be_success
